98b74305c222130f03820ad50aaf45b2755d3531
zhaolong <zhaolong@zhaolongdeMacBook-Pro.local>
2017-03-06 11:06:31 +0800

    update template

--- YiShiShang/YiShiShang/settings.py
+++ YiShiShang/YiShiShang/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = '3uof2k4$9%ye52z#o(+lme!+kfhr3(-!^i0uad-d2%tuzp(vb0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '192.168.95.2',
-    'localhost',
-    '127.0.0.1',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
